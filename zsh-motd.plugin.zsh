0660a95dcc4fc7fdf37f5a39196ee2d5d27d4ea2
Quien Sabe <qs5579@mail.com>
2024-02-13 12:06:41 -0400

    chore: feat: randon login screen, pre-commit support

--- zsh-motd.plugin.zsh
+++ zsh-motd.plugin.zsh
@@ -1,5 +1,6 @@
 # Bash / Ubuntu MOTD for Zsh, but cooler
 # This script is partly based on the default /etc/profile.d/update-motd.sh found in Ubuntu 20.04.
+# shellcheck shell=bash
 
 stamp="${HOME}/.motd_shown"
 always="${HOME}/.update-mode.always"
@@ -48,8 +49,12 @@ print_header() {
     fi
 }
 
+MINUTE_TIME=$(date +%M)
+TRES=$((MINUTE_TIME%3))
+DOS=$((MINUTE_TIME%2))
+
 # Linux MOTD - once a day
-if [ -d /etc/update-motd.d ] && [ ! -e "$HOME/.hushlogin" ] && [ -z "$MOTD_SHOWN" ] && ! find $stamp -newermt 'today 0:00' 2> /dev/null | grep -q -m 1 '.'; then
+if [ -d /etc/update-motd.d ] && [ ! -e "$HOME/.hushlogin" ] && [ -z "$MOTD_SHOWN" ] && [ "$TRES" != "0" ] && [ "$DOS" != "0" ]; then
     [ $(id -u) -eq 0 ] || SHOW="--show-only"
     update-motd $SHOW
     if [ ! -e "$always" ]; then
@@ -57,7 +62,7 @@ if [ -d /etc/update-motd.d ] && [ ! -e "$HOME/.hushlogin" ] && [ -z "$MOTD_SHOWN
       export MOTD_SHOWN=update-motd
     fi
 # ZSH MOTD - once every 3 hours
-elif [ ! -z ${ZSH_MOTD_ALWAYS+x} ] || ! find $stamp -mmin -179 2> /dev/null | grep -q -m 1 '.'; then
+elif [ ! -z ${ZSH_MOTD_ALWAYS+x} ] || [ "$DOS" = "0" ]; then
     print_header
     touch $stamp
 elif [ $PERL_INSTALLED -eq 1 ]; then
